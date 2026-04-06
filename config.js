6870e075f18a49f83f2548719477aee71aaed2ce
easymarketing-pragati <marketing@easyhomefinance.in>
2026-04-06 17:22:52 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ var APP_CONFIG = {
   instagramUrl:  "https://www.instagram.com/easyhomefinance",
   adminPassword: "Pragati@2026",
   gasUrl:        "https://script.google.com/macros/s/AKfycby28WQqptkushJgadmSLgROvzwiZp1OW7XXDmjjSMv9NkkmiSl-lAsvchNaBsPb2kvHwQ/exec",
-  waPhoneId:     "YOUR_WHATSAPP_PHONE_ID",
-  waToken:       "YOUR_WHATSAPP_ACCESS_TOKEN",
+  waPhoneId:     "1022284447639797",
+  waToken:       "EAA5S9V3wMw4BRG2g9OqqzLwzpxMK5qBxBzAAoqlPkAaSSgkeBgKupZBqkKC7YQqDvfmRDg64ImRYSoGvl2jelAepZBOJR4ZAtlMnBtZAbXXjslINihI4ifkYOBIbUHjj1FqXbNOKDV2ivegghWzzOrDKwL3O9EFJHv6CroUoCsvJdJA9n3tMxpXB3MVoPwo6YMuhkzfCA1WsccWfMSFpeb2NT2KZA18ahzgsLMsiMMxX1gi0UX8K9RQ0I3DK4xOZA0ZAMVqngwz3ipUK3t1JOPub7NT4wZDZD",
 };
